f172993bda5d90a745b21c474956a499a486b023
manuel perez <manuelperez.0000@gmail.com>
2024-04-19 12:40:01 -0400

    depositos

--- src/pages/admin/dashboard/depositos.jsx
+++ src/pages/admin/dashboard/depositos.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import useMethods from "../../../hooks/useMethods"
+/* import request from "../../../services/request" */
 
 const Depositos = () => {
 
     const { actualMethods, defaultMethod, setDefaultMethod } = useMethods()
 
-    const [tab, setTab] = useState(1)
+    const [tab, setTab] = useState(4)
 
     const handleMethod = (id) => {
         const newMethod = actualMethods.filter(method => method._id === id)
@@ -15,18 +16,38 @@ const Depositos = () => {
     const handleForm = (e) => {
         e.preventDefault()
 
+        const deposit = {
+            name: e.target.name.value,
+            ci: e.target.ci.value,
+            email: e.target.email.value,
+            phone: e.target.phone.value,
+            operation: e.target.operation.value,
+            account: e.target.account.value,
+            bank: e.target.bank.value,
+            type: e.target.type.value,
+            operationDate: e.target.operationDate.value
+        }
+
+        console.log(deposit)
     }
 
     const Methods = () => {
         return <div className="card p-2">
-            <h3> {defaultMethod.methodName} </h3>
-            <div> {defaultMethod.banco} </div>
-            <div> {defaultMethod.cedula} </div>
-            <div> {defaultMethod.cuenta} </div>
-            <div> {defaultMethod.tipo} </div>
-            <div> {defaultMethod.telefono} </div>
-            <div> {defaultMethod.nombre} </div>
-            <div> {defaultMethod.correo} </div>
+            <h3> {defaultMethod?.methodName} </h3>
+            <div className="flex-between-start">
+                <div>
+                    <div> {defaultMethod?.banco} </div>
+                    <div> {defaultMethod?.cedula} </div>
+                    <div> {defaultMethod?.cuenta} </div>
+                    <div> {defaultMethod?.tipo} </div>
+                    <div> {defaultMethod?.telefono} </div>
+                    <div> {defaultMethod?.nombre} </div>
+                    <div> {defaultMethod?.correo} </div>
+                </div>
+                <div>
+                    <div> <img src={defaultMethod?.imageUrl} height={"70px"} alt="" />  </div>
+                </div>
+            </div>
         </div>
     }
 
@@ -44,66 +65,75 @@ const Depositos = () => {
         {tab === 4 && <div className="p-4">
             <h4>Nuevo deposito</h4>
             <hr />
-            <div className="container-fluid">
-                <div className="row mb-4">
-                    <div className="col-6">
-                        <select onChange={(e) => { handleMethod(e.target.value) }} name="" id="" className="form-select">
-                            {actualMethods.length > 0 && actualMethods.map((method, index) => {
-                                return <option key={index} value={method._id}> {method.methodName}  </option>
-                            })}
-                        </select>
-                    </div>
-                    <div className="col-6">
-                        <Methods />
+            {actualMethods.length > 0 ? <>
+                <div className="container-fluid">
+                    <div className="row mb-4">
+                        <div className="col-6">
+                            <select onChange={(e) => { handleMethod(e.target.value) }} name="" id="" className="form-select">
+                                {actualMethods.length > 0 && actualMethods.map((method, index) => {
+                                    return <option key={index} value={method._id}> {method.methodName}  </option>
+                                })}
+                            </select>
+                        </div>
+                        <div className="col-6">
+                            <Methods />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="card p-3">
-                <form action="" onSubmit={handleForm}>
-                    <div className="flex-between">
-                        <h4>Datos del {defaultMethod.methodName}</h4>
-                        <button className="btn btn-primary">Guardar</button>
-                    </div>
-                    <hr />
-                    <div className="p-4">
+                <div className="card p-3">
+                    <form action="" onSubmit={handleForm}>
+                        <div className="flex-between">
+                            <h4>Datos del {defaultMethod?.methodName}</h4>
+                            <button className="btn btn-primary">Guardar</button>
+                        </div>
+                        <hr />
+                        <div className="p-4">
 
-                        {defaultMethod.banco ? <>
-                            <label htmlFor="banco">Nombre del Banco</label>
-                            <input type="text" name="banco" className="form-control bg-light-gray" placeholder="Ingrese El Banco" />
-                        </> : <input type="hidden" name="banco" />}
+                            {defaultMethod?.banco ? <>
+                                <label htmlFor="bank">Nombre del Banco emisor</label>
+                                <input type="text" name="bank" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Banco" />
+                            </> : <input type="hidden" value="" name="bank" />}
 
-                        {defaultMethod.nombre ? <>
-                            <label htmlFor="nombre">Nombre Completo</label>
-                            <input type="text" name="nombre" className="form-control bg-light-gray" placeholder="Ingrese El Nombre" />
-                        </> : <input type="hidden" name="nombre" />}
+                            {defaultMethod?.nombre ? <>
+                                <label htmlFor="nombre">Nombre Completo de quien envia</label>
+                                <input type="text" name="name" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Nombre" />
+                            </> : <input type="hidden" value="" name="name" />}
 
-                        {defaultMethod.cedula ? <>
-                            <label htmlFor="cedula">Ingrese la cedula</label>
-                            <input type="text" name="cedula" className="form-control bg-light-gray" placeholder="Ingrese El Nombre" />
-                        </> : <input type="hidden" name="cedula" />}
+                            {defaultMethod?.cedula ? <>
+                                <label htmlFor="cedula">Ingrese la cedula del que envia</label>
+                                <input type="text" name="ci" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Nombre" />
+                            </> : <input type="hidden" value="" name="ci" />}
 
-                        {defaultMethod.correo ? <>
-                            <label htmlFor="correo"> Ingrese el correo </label>
-                            <input type="text" name="correo" className="form-control bg-light-gray" placeholder="Ingrese El Nombre" />
-                        </> : <input type="hidden" name="correo" />}
+                            {defaultMethod?.correo ? <>
+                                <label htmlFor="correo"> Ingrese el correo emisor</label>
+                                <input type="text" name="email" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Nombre" />
+                            </> : <input type="hidden" value="" name="email" />}
 
-                        {defaultMethod.cuenta ? <>
-                            <label htmlFor="cuenta">Numero de cuenta</label>
-                            <input type="text" name="cuenta" className="form-control bg-light-gray" placeholder="Ingrese El Nombre" />
-                        </> : <input type="hidden" name="cuenta" />}
+                            {defaultMethod?.cuenta ? <>
+                                <label htmlFor="cuenta">Numero de cuenta bancaria</label>
+                                <input type="text" name="account" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Nombre" />
+                            </> : <input type="hidden" value="" name="account" />}
 
-                        {defaultMethod.tipo ? <>
-                            <label htmlFor="tipo">Tipo de cuenta</label>
-                            <input type="text" name="tipo" className="form-control bg-light-gray" placeholder="Ingrese El Nombre" />
-                        </> : <input type="hidden" name="tipo" />}
+                            {defaultMethod?.tipo ? <>
+                                <label htmlFor="type">Tipo de cuenta bancaria</label>
+                                <input type="text" name="type" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Nombre" />
+                            </> : <input type="hidden" value="" name="type" />}
 
-                        {defaultMethod.telefono ? <>
-                            <label htmlFor="telefono">Telefono</label>
-                            <input type="text" name="telefono" className="form-control bg-light-gray" placeholder="Ingrese El Nombre" />
-                        </> : <input type="hidden" name="telefono" />}
-                    </div>
+                            {defaultMethod?.telefono ? <>
+                                <label htmlFor="telefono">Telefono emisor</label>
+                                <input type="text" name="phone" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Nombre" />
+                            </> : <input type="hidden" value="" name="phone" />}
 
-                    {/* banco
+                            <label htmlFor=""> Numero de operacion </label>
+                            <input type="text" name="operation" className="form-control bg-light-gray mb-4" required placeholder="Ingrese El Nombre" />
+
+                            <div className="form-date">
+                                <label htmlFor="operationDate" className="fecha-envio-labek">Fecha de envio</label>
+                                <input type="date" name="operationDate" className="form-control bg-light-gray mb-4 mx-3" required placeholder="Ingrese El Nombre" />
+                            </div>
+                        </div>
+
+                        {/* banco
                     nombre
                     cedula
                     correo
@@ -112,8 +142,12 @@ const Depositos = () => {
                     telefono
                     tipo */}
 
-                </form>
-            </div>
+                    </form>
+                </div>
+            </> : <div className="text-center">
+                <h3 className="py-4 bg-dark text-light rounded-3">No hay metodos de pagos</h3>
+                <p>Por favor dirijase a la seccion de metodos de pago e ingrese sus <b> metodos de pago </b> </p>
+            </div>}
         </div >}
 
         {
