4a47ff1b1d542f6f9dafe6629155a39943efe61d
manuel perez <manuelperez.0000@gmail.com>
2024-05-20 15:15:24 -0400

    reparacion de todos los desastres de json

--- src/components/menuClient/MenuClient.jsx
+++ src/components/menuClient/MenuClient.jsx
@@ -8,49 +8,50 @@ import userStore from '../../store/userStore'
 
 // eslint-disable-next-line react/prop-types
 const MenuClient = ({ children }) => {
-    const [expanded, setExpanded] = useState(false);
-    const [activePath, setActivePath] = useState('');
-    const navigate = useNavigate();
-    const location = useLocation();
+
+    const [expanded, setExpanded] = useState(false)
+    const [activePath, setActivePath] = useState('')
+    const navigate = useNavigate()
+    const location = useLocation()
     const { setUser } = userStore()
 
-    const handleToggleMenu = () => {
-        setExpanded(!expanded);
-    };
+    const handleToggleMenu = () => setExpanded(!expanded)
 
     const handleClose = () => {
-        localStorage.removeItem('user');
+        localStorage.removeItem('user')
         setUser({ _id: '', level: 0 })
-        navigate('/login');
-        return null;
+        navigate('/login')
+        return null
     }
 
     useEffect(() => {
-        setActivePath(location.pathname);
-    }, [location]);
+        setActivePath(location.pathname)
+        setExpanded(!expanded)
+        console.log(location.pathname)
+    }, [location])
 
     return (
-        <div className='container-fluid bg-granja'>
-            <Navbar bg="light" expand="md" className="d-block d-md-none">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggleMenu} />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/lobby">Lobby</Nav.Link>
-                        <Nav.Link as={Link} to="/history">Historial de Jugadas</Nav.Link>
-                        <Nav.Link as={Link} to="/transactions">Transacciones</Nav.Link>
-                        <Nav.Link as={Link} to="/perfil">Perfil</Nav.Link>
-                        <Nav.Link as={Link} to="/support">Soporte</Nav.Link>
-                        <button onClick={handleClose}>Cerrar Sesion</button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-
-            <div className={`${styles.containerRow} row`}>
-                <div className={`d-none d-md-block sidebar ${styles.bgSidebar}`}>
-                    <div className={styles.sidebarSticky}>
-                        <div className="row position-relative">
+        <div className='container-fluid bg-granja p-0' >
+            <div className="row g-0">
+                <div className='col-12'>
+                    <Navbar expand="md" className="d-block d-md-none p-2">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-light' onClick={() => handleToggleMenu()} />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto text-light mt-3">
+                                <Nav.Link as={Link} to="/lobby" className='text-light'> <i className='bi bi-controller px-1' /> Lobby</Nav.Link>
+                                <Nav.Link as={Link} to="/history" className='text-light'><i className='bi bi-play px-1' /> Historial de Jugadas</Nav.Link>
+                                <Nav.Link as={Link} to="/transactions" className='text-light'><i className='bi bi-list px-1' /> Transacciones</Nav.Link>
+                                <Nav.Link as={Link} to="/perfil" className='text-light'><i className='bi bi-people px-1' /> Perfil</Nav.Link>
+                                <Nav.Link as={Link} to="/support" className='text-light'><i className='bi bi-envelope px-1' /> Soporte</Nav.Link>
+                                <button className='btn btn-light mt-3' onClick={() => handleClose()}>Cerrar Sesion</button>
+                                <hr className='text-light' />
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                    <div className={`${styles.bgSidebar} d-none d-md-block `}>
+                        <div className='p-2'>
                             <Nav.Link as={Link} to="/">
-                                <img src={logo} alt="Logo" className="mt-4" style={{ position: 'absolute', width: '100%', maxWidth: '150px' }} />
+                                <img src={logo} alt="Logo" className="mt-4 w-100" />
                             </Nav.Link>
                         </div>
                         <Nav className={`${styles.sidebar} flex-column`}>
@@ -69,19 +70,19 @@ const MenuClient = ({ children }) => {
                             <Nav.Link as={Link} to="/support">
                                 <i className={`bi bi-headphones ${activePath === '/support' && 'active'}`}></i>
                             </Nav.Link>
-                            <Nav.Link onClick={handleClose}>
+                            <Nav.Link onClick={() => handleClose()}>
                                 <i className="bi bi-door-open"></i>
                             </Nav.Link>
                         </Nav>
                     </div>
-                </div>
-
-                <div className={styles.content}>
-                    {children}
+                    <div className={styles.content}>
+                        {children}
+                    </div>
                 </div>
             </div>
         </div>
-    );
-};
 
-export default MenuClient;
+    )
+}
+
+export default MenuClient
