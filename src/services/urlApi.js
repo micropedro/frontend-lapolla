8de37cf4ab18f74d46ed42c9a252fc310b373a5e
manuel perez <manuelperez.0000@gmail.com>
2024-05-21 17:38:37 -0400

    path production

--- src/services/urlApi.js
+++ src/services/urlApi.js
@@ -1,5 +1,6 @@
-import { pathLocale, pathProduction } from '../config.json'
-const env = import.meta.env.VITE_DEVELOPMENT
-const urlApi = env === "true" ? pathLocale : pathProduction
-
+/* import { pathLocale } from '../config.json' */
+import { pathProduction } from '../config.json'
+/* const env = import.meta.env.VITE_DEVELOPMENT */
+//const urlApi = env === "true" ? pathLocale : pathProduction
+const urlApi = pathProduction
 export default urlApi
