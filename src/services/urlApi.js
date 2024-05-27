f7f1fe77638f30f83ef8ee3e174c7a9face8065a
manuel perez <manuelperez.0000@gmail.com>
2024-05-27 16:03:31 -0400

    metodos de pago eliminados

--- src/services/urlApi.js
+++ src/services/urlApi.js
@@ -1,6 +1,5 @@
 //develop
 /* import { pathLocale, pathProduction } from '../config.json'
-
 const env = import.meta.env.VITE_DEVELOPMENT
 const urlApi = env === "true" ? pathLocale : pathProduction */
 
