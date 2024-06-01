c17fd9f33833423f630e4a1776872ddfdb8fda09
manuel perez <manuelperez.0000@gmail.com>
2024-06-01 12:56:58 -0400

    depositos arreglado

--- src/pages/admin/dashboard/recargas/recargas.jsx
+++ src/pages/admin/dashboard/recargas/recargas.jsx
@@ -1,3 +1,4 @@
+import { Spinner } from "react-bootstrap";
 import Guard from "../../../../components/Guard";
 import useRecargas from "../../../../hooks/useRecargas"
 import permisions from "../../../../services/permissions";
@@ -5,7 +6,7 @@ import Modal from "./modal";
 const Recargas = () => {
 
     const { userRecharge, setUserCi, userCi, getUserToRecharge, setModal,
-        confirmRecharge, setAmountToRecharge, amountToRecharge } = useRecargas()
+        confirmRecharge, setAmountToRecharge, amountToRecharge, loading } = useRecargas()
 
     if (permisions.permit(9)) return (
         <Guard>
@@ -13,20 +14,22 @@ const Recargas = () => {
             <div className='flex-between px-4 pt-3'>
                 <h2> Recargas </h2>
                 <div>
-                    <div className="input-group mb-3">
-                        <input value={userCi} onChange={(e) => setUserCi(e.target.value)} type="text" className="form-control" placeholder="Ingrese la cedula" aria-label="Ingrese la cedula" aria-describedby="basic-addon2" />
-                        <div className="input-group-append">
-                            {userCi ? <>
-                                <button onClick={getUserToRecharge} className="btn btn-primary" type="button"> Buscar </button>
-                            </> : <>
-                                <button disabled className="btn btn-primary" type="button" > Buscar </button>
-                            </>}
+                    <form onSubmit={getUserToRecharge}>
+                        <div className="input-group mb-3">
+                            <input value={userCi} onChange={(e) => setUserCi(e.target.value)} type="text" className="form-control" placeholder="Ingrese la cedula" aria-label="Ingrese la cedula" aria-describedby="basic-addon2" />
+                            <div className="input-group-append">
+                                {userCi ? <>
+                                    <input type='submit' className="btn btn-primary" value="Buscar" />
+                                </> : <>
+                                    <button disabled className="btn btn-primary" type="button" > Buscar </button>
+                                </>}
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
             <hr />
-            {userRecharge ? <>
+            {loading ? <div className="text-center p-4"><Spinner /></div> : userRecharge ? <>
                 <div className="p-5">
                     <div className="card p-4">
                         <div className="d-flex">
@@ -35,20 +38,17 @@ const Recargas = () => {
                         <div className="d-flex">
                             <b className="mx-2">Cedula:</b> {userRecharge.ci}
                         </div>
-                        <div className="d-flex">
+                        {/* <div className="d-flex">
                             <b className="mx-2">Saldo:</b> {userRecharge.balance}
-                        </div>
+                        </div> */}
                         <hr />
-                        <div className="d-flex">
+                        <div className="flex-between">
                             <h4 className="mx-2">Monto A recargar (Bs)</h4>
-                            <input onChange={(e) => setAmountToRecharge(e.target.value)} className="form-control" type="number" step={0.01} value={amountToRecharge} />
-                        </div>
-                        <div className="my-4 text-end">
-                            {amountToRecharge ? <>
-                                <button onClick={() => setModal(true)} className="btn btn-primary"> Recargar </button>
-                            </> : <>
-                                <button disabled className="btn btn-primary"> Recargar </button>
-                            </>}
+                            <input onChange={(e) => setAmountToRecharge(e.target.value)} className="form-control mx-4" type="number" step={0.01} value={amountToRecharge} />
+                            <div className="my-4 text-end">
+                                {amountToRecharge ? <button onClick={() => setModal(true)} className="btn btn-primary"> Recargar </button>
+                                    : <button disabled className="btn btn-primary"> Recargar </button>}
+                            </div>
                         </div>
                     </div>
                 </div>
