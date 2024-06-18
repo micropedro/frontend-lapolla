ef4e3512359791cba226e32dea331a4c459f12ae
manuel perez <manuelperez.0000@gmail.com>
2024-06-18 00:10:41 -0400

    bot

--- src/pages/admin/dashboard/qunielas/pastQuinielas.jsx
+++ src/pages/admin/dashboard/qunielas/pastQuinielas.jsx
@@ -1,6 +1,7 @@
 import { Spinner } from "react-bootstrap"
 import useQuinielas from "../../../../hooks/useQuinielas"
 import useLoadingStore from "../../../../store/loadingStore"
+import { formatDate2 } from "../../../../services/formatDate"
 const PastQuinielas = () => {
     const { loading } = useLoadingStore()
     const { quinielas } = useQuinielas()
@@ -9,6 +10,7 @@ const PastQuinielas = () => {
             <thead>
                 <tr>
                     <th>ID</th>
+                    <th>Fecha</th>
                     <th>Tipo</th>
                     <th>Ganadores</th>
                     <th>Precio</th>
@@ -21,6 +23,7 @@ const PastQuinielas = () => {
                 {quinielas.length > 0 && quinielas.map((quiniela, index) => (
                     <tr key={index} className={quiniela.tipoQuiniela === 1 ? "tr-gran-quiniela" : "tr-mini-quiniela"}>
                         <td>{quiniela._id.slice(-6)}</td>
+                        <td>{formatDate2(quiniela.fechaQuiniela)}</td>
                         <td>{quiniela.tipoQuiniela === 1 ? <> Gran </> : <> Mini </>} Quiniela</td>
                         <td>{quiniela.winners.length} <i className="btn bi bi-eye-fill text-primary p-0" /> </td>
                         <td>{quiniela.precioQuiniela}</td>
