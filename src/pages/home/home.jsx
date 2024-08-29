e03a1482c6d381b782f7d60ac17c58fef3ce3fb6
manuel perez <manuelperez.0000@gmail.com>
2024-08-29 09:03:57 -0400

    prod 3

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,23 +5,12 @@ import NextQuiniela from './nextQuiniela'
 import ItemsRoulettes from './itemsRoulettes'
 import Footer from './footer'
 import Carrousels from '../../components/carrousels/Carrousels'
-import images from '../../images/images'
 import PollasActuales from '../../components/pollasActuales/pollasActuales'
-import { useState } from 'react'
-
-
-const Prod = () => {
-    const [prod, setProd] = useState(true)
-    if (prod) return <div className='p-1 bg-success flex-between text-light'>
-        <div>Prod</div>
-        <button onClick={() => setProd(false)} className='btn btn-sm btn-danger'> x </button>
-    </div>
-}
 
 const Home = () => {
     return (
         <div className='bg-granja-2'>
-            <Prod />
+           
             <Menu />
             <div className="container pt-4 carrousel">
                 <Carrousels />
